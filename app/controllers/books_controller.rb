e82e7262cdfcdc47f3a229dbcb19bb5a7260f750
penpentan <66662244+penpentan@users.noreply.github.com>
2023-04-10 03:52:22 +0000

    [Finish] 3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,49 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice]="Book was successfully created."
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
   def index
+    @book=Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book =Book.find(params[:id])
+  end
+  def update
+    @book=Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to "/books/#{@book.id}"
+    else
+      render :show
+    end
+  end
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy # delete record
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
---
+++ config/initializers/rack_profile.rb
@@ -0,0 +1,3 @@
+# # 参考
+# # https://prograshi.com/framework/rails/remove-ms-by-rack-mini-profiler/
+# Rack::MiniProfiler.config.start_hidden = true
